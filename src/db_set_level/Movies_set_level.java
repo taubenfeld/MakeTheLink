51de11eae3f65bdd9fee7fc20dbdf4d323143e4f
user7 <schwart7@mail.tau.ac.il>
2014-05-30 01:09:19 +0300

    \

--- src/db_set_level/Movies_set_level.java
+++ src/db_set_level/Movies_set_level.java
@@ -30,27 +30,7 @@ public class Movies_set_level {
 "			inner join curr_cinema_tags t on t.id=mt.tag_id	" +
 "			inner join	" +
 "				(select am.movie_id from curr_cinema_actor_movie am group by movie_id having count(*)>3) am	" +
-"				on am.movie_id=m.id	" +
-"			where	0=1 " +
-				category_comment[0] + "	or	t.name='action'	" + category_uncomment[0] + "	" +
-				category_comment[1] + "	or 	t.name='adventure'	" + category_uncomment[1] + "	" +
-				category_comment[2] + "	or 	t.name='animated'	" + category_uncomment[2] + "	" +
-				category_comment[3] + "	or 	t.name='comedy'	" + category_uncomment[3] + "	" +
-				category_comment[4] + "	or 	t.name='crime'	" + category_uncomment[4] + "	" +
-				category_comment[5] + "	or 	t.name='documentary'	" + category_uncomment[5] + "	" +
-				category_comment[6] + "	or 	t.name='drama'	" + category_uncomment[6] + "	" +
-				category_comment[7] + "	or 	t.name='fantasy'	" + category_uncomment[7] + "	" +
-				category_comment[8] + "	or 	t.name='horror'	" + category_uncomment[8] + "	" + "	" +
-				category_comment[9] + "	or 	t.name='musical'	" + category_uncomment[9] + "	" +
-				category_comment[10]+ "	or 	t.name='mystery'	" + category_uncomment[10] + "	" +
-				category_comment[11]+ "	or 	t.name='romance'	" + category_uncomment[11] + "	" +
-				category_comment[12]+ "	or 	t.name='science_fiction'	" + category_uncomment[12] + "	" +
-				category_comment[13]+ "	or 	t.name='sports'	" + category_uncomment[13] + "	" +
-				category_comment[14]+ "	or 	t.name='teen'	" + category_uncomment[14] + "	" +
-				category_comment[15]+ "	or 	t.name='television'	" + category_uncomment[15] + "	" +
-				category_comment[16]+ "	or 	t.name='thriller'	" + category_uncomment[16] + "	" +
-				category_comment[17]+ "	or 	t.name='war'	" + category_uncomment[17] +  "	" +
-				category_comment[18]+ "	or 	t.name='western'	" + category_uncomment[18] + "	");
+"				on am.movie_id=m.id	");
 		
 		rst.next();
 		int num_rows = rst.getInt(1);
@@ -67,26 +47,6 @@ public class Movies_set_level {
 "				inner join 	" +
 "					(select am.movie_id from curr_cinema_actor_movie am group by movie_id having count(*)>3) am	" +
 "					on am.movie_id=m.id	" +
-"				where  1=0 " +
-				category_comment[0] + "	or	t.name='action'	" + category_uncomment[0] + "	" +
-				category_comment[1] + "	or 	t.name='adventure'	" + category_uncomment[1] + "	" +
-				category_comment[2] + "	or 	t.name='animated'	" + category_uncomment[2] + "	" +
-				category_comment[3] + "	or 	t.name='comedy'	" + category_uncomment[3] + "	" +
-				category_comment[4] + "	or 	t.name='crime'	" + category_uncomment[4] + "	" +
-				category_comment[5] + "	or 	t.name='documentary'	" + category_uncomment[5] + "	" +
-				category_comment[6] + "	or 	t.name='drama'	" + category_uncomment[6] + "	" +
-				category_comment[7] + "	or 	t.name='fantasy'	" + category_uncomment[7] + "	" +
-				category_comment[8] + "	or 	t.name='horror'	" + category_uncomment[8] + "	" +
-				category_comment[9] + "	or 	t.name='musical'	" + category_uncomment[9] + "	" +
-				category_comment[10]+ "	or 	t.name='mystery'	" + category_uncomment[10] + "	" +
-				category_comment[11]+ "	or 	t.name='romance'	" + category_uncomment[11] + "	" +
-				category_comment[12]+ "	or 	t.name='science_fiction'	" + category_uncomment[12] + "	" +
-				category_comment[13]+ "	or 	t.name='sports'	" + category_uncomment[13] + "	" +
-				category_comment[14]+ "	or 	t.name='teen'	" + category_uncomment[14] + "	" +
-				category_comment[15]+ "	or 	t.name='television'	" + category_uncomment[15] + "	" +
-				category_comment[16]+ "	or 	t.name='thriller'	" + category_uncomment[16] + "	" +
-				category_comment[17]+ "	or 	t.name='war'	" + category_uncomment[17] +  "	" +
-				category_comment[18]+ "	or 	t.name='western'	" + category_uncomment[18] + "	" +
 "				order by num_links desc	" +
 "				limit "+limit.toString()+") x);");
 
