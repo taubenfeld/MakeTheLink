6c43e231d35f7be85c265866245e85e8bd05b6c6
user7 <schwart7@mail.tau.ac.il>
2014-05-30 10:54:53 +0300

    s

--- src/App/Question.java
+++ src/App/Question.java
@@ -6,6 +6,30 @@ public class Question {
 	private String[] hintsList;
 	private String correctAnswer;
 	
+	public void setAnswer(String answer){
+		correctAnswer=answer;
+	}
+	
+	public void setAnswerOptions(String[] answerOps){
+		answersOptions=answerOps;
+	}
+	
+	public void setHintsList(String[] hintLst){
+		hintsList=hintLst;
+	}
+	
+	public String getAnswer(String answer){
+		return correctAnswer;
+	}
+	
+	public String[] getAnswerOptions(String[] answerOps){
+		return answersOptions;
+	}
+	
+	public String[] getHintsList(String[] hintLst){
+		return hintsList;
+	}
+	
 	public Question(){
 		
 	}
